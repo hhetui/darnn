87119ada848b0699a9626a22f59e81027a69cb3e
wxk <sy1909134@buaa.edu.cn>
2021-05-09 20:40:39 +0800

    base_model

--- README.md
+++ README.md
@@ -15,11 +15,12 @@
 - ver6_2      改变隐向量长度 128
 - ver6_3      改变time_step 30
 - ver6_4      改变time_step 10
+- ver6_5      尝试独立的带dropout的selfattention模型
 
 - ver7        在DARNN+selfattention的ENDN中先用self-attention处理数据
 - ver8        将DARNN+selfattention的attention部分直接替换为self-attention
 - ver9        将DARNN+selfattention的attention部分直接替换为师兄给的CNNself-attention
 
--ver10        将DARNN+selfattention的attention部分直接替换为师兄给的第二篇CNN的down-sampling部分过拟合增加正则化项
--ver10_1      增加dropout 0.3
--ver10_2      将隐藏层128减少到32
+- ver10        将DARNN+selfattention的attention部分直接替换为师兄给的第二篇CNN的down-sampling部分过拟合增加正则化项
+- ver10_1      增加dropout 0.3
+- ver10_2      将隐藏层128减少到32
---
+++ myversion/Base_model.py
@@ -0,0 +1,46 @@
+import sys
+import math
+import random
+import argparse
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from utils import get_opt
+from trainer import Trainer
+
+class SelfAttention(nn.Module):
+    '''
+    若X: T*F Mask应为 T*T
+    '''
+    def __init__(self, input_size, output_size,drop_ratio = 0):
+        super(SelfAttention, self).__init__()
+        self.Dropout = nn.Dropout(drop_ratio)
+
+        self.wq = nn.Linear(in_features=input_size,
+                            out_features=output_size, bias=False)
+        self.wk = nn.Linear(in_features=input_size,
+                            out_features=output_size, bias=False)
+        self.wv = nn.Linear(in_features=input_size,
+                            out_features=output_size, bias=False)
+
+    def forward(self, X, Mask = None):
+        #X : B * T * input_size
+        #Mask:T*T
+        #qkv:B*T*output_size
+        q = self.wq(X)
+        k = self.wk(X)
+        v = self.wv(X)
+
+        dk = q.size(-1)
+        #z: B*T*T
+        z = torch.bmm(q, k.permute(0,2,1)) / math.sqrt(dk)
+        if Mask:
+            z = z.masked_fill_(Mask>0,-np.inf)
+        beta = F.softmax(z, dim=2)
+        beta = self.Dropout(beta)
+
+        res = torch.bmm(beta, v)
+        return res
