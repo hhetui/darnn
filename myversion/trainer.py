4b4647b2b417ba9461b292c3c2e20e70f8e5be9f
wxk <sy1909134@buaa.edu.cn>
2021-05-18 20:00:10 +0800

    重构数据生成类

--- myversion/trainer.py
+++ myversion/trainer.py
@@ -12,14 +12,13 @@ import matplotlib.pyplot as plt
 import torch
 from torch import optim
 from torch.autograd import Variable
-from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 
-from utils import get_opt, get_logger, load_dataset, Dataset_generate
+from utils import get_opt, get_logger, DataLoader_Generate
 
 
 class Trainer:
@@ -54,7 +53,7 @@ class Trainer:
         self.model = myModel(**self.model_conf)
         self.load_checkpoint()
         self.logger.info('导入数据集......')
-        self.Data = load_dataset(self.data_conf)
+        self.DataLoader_Generate = DataLoader_Generate(self.train_conf,self.data_conf)
         self.logger.info('数据集导入成功！')
         self.scheduler = ReduceLROnPlateau(
             self.optimizer, mode='min', factor=0.6, patience=self.train_conf['patience'], verbose=True, min_lr=0)
@@ -97,85 +96,84 @@ class Trainer:
             self.acc_test_max_diff = 0
             self.result = defaultdict(list)
 
-    def run(self):
-        Train_Val_data = pd.DataFrame(self.Data['train'])
-        Test_data = pd.DataFrame(self.Data['test'])
+    def save_checkpoint(self,best):
         
-        TestDataloader = DataLoader(
-            Dataset_generate(self.data_conf['dataset_type'], Test_data), batch_size=self.train_conf['batch'], shuffle=False)
+        self.result['epoch'].append(self.cur_epoch)
+        self.result['loss'].append(self.epoch_Loss)
+        self.result['lr'].append(self.optimizer.state_dict()[
+                                'param_groups'][0]['lr'])
+        self.result['train_accuracy'].append(self.train_accuracy)
+        self.result['acc_train_max_diff'].append(
+            self.acc_train_max_diff)
+        self.result['validation_accuracy'].append(self.validation_accuracy)
+        self.result['validation_loss'].append(self.val_loss)
+        self.result['acc_val_max_diff'].append(self.acc_val_max_diff)
+        self.result['test_random'].append(self.test_random)
+        self.result['test_accuarcy'].append(self.test_accuracy)
+        self.result['test_loss'].append(self.test_loss)
+        self.result['acc_test_max_dif'].append(self.acc_test_max_diff)
+        
+        #保存csv结果
+        pd.DataFrame(self.result).to_csv(self.csv_name, index=False)
+        #保存模型参数以及一些max数据
+        torch.save(
+            {
+                "epoch": self.cur_epoch,
+                "epoch_Loss:": self.epoch_Loss,
+                "acc_train_max_diff": self.acc_train_max_diff,
+                "acc_val_max_diff": self.acc_val_max_diff,
+                "test_best_loss": self.test_best_loss,
+                "best_model_test_acc_diff": self.best_model_test_acc_diff,
+                "acc_test_max_diff": self.acc_test_max_diff,
+                "model_state_dict": self.model.state_dict(),
+                "optim_state_dict": self.optimizer.state_dict(),
+            },
+            os.path.join(self.result_path,"last.pt"))
+        if best:
+            torch.save(
+            {
+                "epoch": self.cur_epoch,
+                "epoch_Loss:": self.epoch_Loss,
+                "acc_train_max_diff": self.acc_train_max_diff,
+                "acc_val_max_diff": self.acc_val_max_diff,
+                "test_best_loss": self.test_best_loss,
+                "best_model_test_acc_diff": self.best_model_test_acc_diff,
+                "acc_test_max_diff": self.acc_test_max_diff,
+                "model_state_dict": self.model.state_dict(),
+                "optim_state_dict": self.optimizer.state_dict(),
+            },
+            os.path.join(self.result_path,"best.pt"))
+        #绘制折线图
+        self.save_figure()
+
+    def run(self):
+        self.TestDataloader = self.DataLoader_Generate.Get_TestDataLoader()
 
         self.no_impr = 0
         while(self.cur_epoch < self.train_conf['epoch']):
 
-            Train_data = Train_Val_data.sample(frac=1-self.train_conf['split'])
-            Train_data.sort_index(inplace=True)
-            Val_data = Train_Val_data[~Train_Val_data.index.isin(
-                Train_data.index)]
-            Val_data.sort_index(inplace=True)
-
-            TrainDataloader = DataLoader(
-                Dataset_generate(self.data_conf['dataset_type'], Train_data), batch_size=self.train_conf['batch'], shuffle=False)
-            ValDataloader = DataLoader(
-                Dataset_generate(self.data_conf['dataset_type'], Val_data), batch_size=self.train_conf['batch'], shuffle=False)
+            self.TrainDataloader,self.ValDataloader = self.DataLoader_Generate.Get_Train_ValLoader()
 
             self.cur_epoch += 1
             self.logger.info('======epoch:'+str(self.cur_epoch) +
                              ' 正在训练 ========================>')
-            train_accuracy = self.train(TrainDataloader)
-            validation_accuracy, val_loss = self.valid(ValDataloader)
-            test_accuracy, test_random, test_loss = self.test(TestDataloader)
-
-            def save_checkpoint(best):
-                if not best:
-                    self.result['epoch'].append(self.cur_epoch)
-                    self.result['loss'].append(self.epoch_Loss)
-                    self.result['lr'].append(self.optimizer.state_dict()[
-                                            'param_groups'][0]['lr'])
-                    self.result['train_accuracy'].append(train_accuracy)
-                    self.result['acc_train_max_diff'].append(
-                        self.acc_train_max_diff)
-                    self.result['validation_accuracy'].append(validation_accuracy)
-                    self.result['validation_loss'].append(val_loss)
-                    self.result['acc_val_max_diff'].append(self.acc_val_max_diff)
-                    self.result['test_random'].append(test_random)
-                    self.result['test_accuarcy'].append(test_accuracy)
-                    self.result['test_loss'].append(test_loss)
-                    self.result['acc_test_max_dif'].append(self.acc_test_max_diff)
-                if not os.path.exists(self.result_path):
-                    self.logger.info('第一次保存，新建目录:', self.result_path)
-                    os.mkdir(self.result_path)
-                #保存csv结果
-                pd.DataFrame(self.result).to_csv(self.csv_name, index=False)
-                #保存模型参数以及一些max数据
-                torch.save(
-                    {
-                        "epoch": self.cur_epoch,
-                        "epoch_Loss:": self.epoch_Loss,
-                        "acc_train_max_diff": self.acc_train_max_diff,
-                        "acc_val_max_diff": self.acc_val_max_diff,
-                        "test_best_loss": self.test_best_loss,
-                        "best_model_test_acc_diff": self.best_model_test_acc_diff,
-                        "acc_test_max_diff": self.acc_test_max_diff,
-                        "model_state_dict": self.model.state_dict(),
-                        "optim_state_dict": self.optimizer.state_dict(),
-                    },
-                    os.path.join(self.result_path,
-                                 "{0}.pt".format("best" if best else "last")))
-                #绘制折线图
-                self.save_figure()
+            self.train()
+            self.valid()
+            self.test()
 
             if test_loss < self.test_best_loss:
-                save_checkpoint(best=True)
-                self.test_best_loss = test_loss
-                self.best_model_test_acc_diff = test_accuracy - test_random
+                self.test_best_loss = self.test_loss
+                self.best_model_test_acc_diff = self.test_accuracy - self.test_random
                 self.no_impr = 0
                 self.logger.info('Epoch: {:d}, now best test loss change: {:.8f}'.format(
                     self.cur_epoch, self.test_best_loss))
+                self.save_checkpoint(best=True)
             else:
+                best = False
                 self.no_impr += 1
                 self.logger.info('{:d} no improvement, best loss: {:.4f}'.format(
                     self.no_impr, self.scheduler.best))
-            save_checkpoint(best=False)
+                self.save_checkpoint(best=False)
 
             if self.no_impr == self.train_conf['stop']:
                 self.show_result()
@@ -184,13 +182,13 @@ class Trainer:
                 break
         self.show_result()
 
-    def train(self, TrainDataloader):
+    def train(self):
         self.logger.info('\033[1;34m Train: \033[0m')
         train_loss = 0
         t_pred = []
         t_ori = []
         self.model.train()
-        for _, sample in enumerate(TrainDataloader):
+        for _, sample in enumerate(self.TrainDataloader):
 
             self.optimizer.zero_grad()
 
@@ -209,74 +207,71 @@ class Trainer:
             train_loss += loss.data.item()
         
         self.epoch_Loss = train_loss/len(t_ori)
-        train_accuracy, precision, recall, f1 = self.metrics(t_pred, t_ori)
+        self.train_accuracy, precision, recall, f1 = self.metrics(t_pred, t_ori)
         train_random = self.rand_acc(t_ori)
         self.acc_train_max_diff = max(
-            self.acc_train_max_diff, train_accuracy-train_random)
+            self.acc_train_max_diff, self.train_accuracy-train_random)
         self.logger.info('第 \033[1;34m %d \033[0m 轮的训练集正确率为:\033[1;32m %.4f \033[0m epoch_mean_Loss 为: \033[1;32m %.8f \033[0m' %
-                         (self.cur_epoch, train_accuracy, self.epoch_Loss))
+                         (self.cur_epoch, self.train_accuracy, self.epoch_Loss))
         self.logger.info('\033[1;31m Accuracy:%.4f Precision:%.4f Recall:%.4f F1:%.4f \033[0m' % (
-            train_accuracy, precision, recall, f1))
+            self.train_accuracy, precision, recall, f1))
         self.logger.info('\033[1;31m Random:%.4f\tMaxAccDiff:%.6f \033[0m' %
                          (train_random, self.acc_train_max_diff))
-        return train_accuracy
-
-    def valid(self, ValDataloader):
+        
+    def valid(self):
         self.logger.info('\033[1;34m Valid: \033[0m')
         with torch.no_grad():
             t_pred = []
             t_ori = []
-            val_loss = 0
+            self.val_loss = 0
             self.model.eval()
-            for _, sample in enumerate(ValDataloader):
+            for _, sample in enumerate(self.ValDataloader):
                 var_x, var_y, var_t = self.get_xy_from_sample(sample)
                 out = self.model(var_x, var_y)
                 pre_t = (out >= 0.5) + 0
-                val_loss += self.model.loss_func(out, var_t).data.item()
+                self.val_loss += self.model.loss_func(out, var_t).data.item()
                 t_pred.extend(pre_t.data.cpu().numpy())
                 t_ori.extend(var_t.cpu().numpy())
 
-            val_loss = val_loss/len(t_ori)
-        validation_accuracy, precision, recall, f1 = self.metrics(
+            self.val_loss = self.val_loss/len(t_ori)
+        self.validation_accuracy, precision, recall, f1 = self.metrics(
             t_pred, t_ori)
         validation_random = self.rand_acc(t_ori)
 
-        self.scheduler.step(val_loss)
+        self.scheduler.step(self.val_loss)
         sys.stdout.flush()
         self.acc_val_max_diff = max(
-            self.acc_val_max_diff, validation_accuracy-validation_random)
+            self.acc_val_max_diff, self.validation_accuracy-validation_random)
         self.logger.info('\033[1;31m Accuracy:%.4f Precision:%.4f Recall:%.4f F1:%.4f val_loss:%.8f \033[0m' % (
-            validation_accuracy, precision, recall, f1, val_loss))
+            self.validation_accuracy, precision, recall, f1, self.val_loss))
         self.logger.info('\033[1;31m Random:%.4f\tMaxAccDiff:%.6f \tbest_model_test_acc_diff:%.6f \033[0m' %
                          (validation_random, self.acc_val_max_diff, self.best_model_test_acc_diff))
-        return validation_accuracy, val_loss
-
-    def test(self, TestDataloader):
+        
+    def test(self):
         self.logger.info('\033[1;34m Test: \033[0m')
         with torch.no_grad():
             t_pred = []
             t_ori = []
             self.model.eval()
-            test_loss = 0
-            for _, sample in enumerate(TestDataloader):
+            self.test_loss = 0
+            for _, sample in enumerate(self.TestDataloader):
                 var_x, var_y, var_t = self.get_xy_from_sample(sample)
 
                 out = self.model(var_x, var_y)
                 pre_t = (out >= 0.5) + 0
-                test_loss += self.model.loss_func(out, var_t).data.item()
+                self.test_loss += self.model.loss_func(out, var_t).data.item()
                 t_pred.extend(pre_t.data.cpu().numpy())
                 t_ori.extend(var_t.cpu().numpy())
-            test_loss = test_loss/len(t_ori)
-        test_accuracy, precision, recall, f1 = self.metrics(t_pred, t_ori)
-        test_random = self.rand_acc(t_ori)
+            self.test_loss = self.test_loss/len(t_ori)
+        self.test_accuracy, precision, recall, f1 = self.metrics(t_pred, t_ori)
+        self.test_random = self.rand_acc(t_ori)
         self.acc_test_max_diff = max(
-            self.acc_test_max_diff, test_accuracy-test_random)
+            self.acc_test_max_diff, self.test_accuracy-self.test_random)
         self.logger.info('\033[1;31m Accuracy:%.4f Precision:%.4f Recall:%.4f F1:%.4f test_loss:%.8f \033[0m' % (
-            test_accuracy, precision, recall, f1, test_loss))
+            self.test_accuracy, precision, recall, f1, self.test_loss))
         self.logger.info('\033[1;31m Random:%.4f\ttestMaxAccDiff:%.6f \033[0m' %
-                         (test_random, self.acc_test_max_diff))
-        return test_accuracy, test_random, test_loss
-
+                         (self.test_random, self.acc_test_max_diff))
+        
     def get_xy_from_sample(self, sample):
         var_x = self.to_Tensor(sample[0])
         var_y = self.to_Tensor(sample[1])
@@ -298,21 +293,21 @@ class Trainer:
         sns.set()
         plt.rcParams['lines.linewidth']=0.3
         plt.switch_backend('agg')
-        train_loss = plt.plot(self.result['epoch'],self.result['loss'],'lightcoral',label='loss')
-        test_loss = plt.plot(self.result['epoch'],self.result['test_loss'],'lightseagreen',label='test_loss')
+        train_Loss = plt.plot(self.result['epoch'],self.result['loss'],'lightcoral',label='loss')
+        test_Loss = plt.plot(self.result['epoch'],self.result['test_loss'],'lightseagreen',label='test_loss')
         plt.title('Loss_figure')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
         plt.legend()
-        plt.savefig(os.path.join(self.result_path,self.model_file_name+'_Loss_figure.jpg'),dpi = 2000)
+        plt.savefig(os.path.join(self.result_path,self.model_file_name+'_Loss_figure.jpg'),dpi = 1500)
         plt.clf()
-        train_loss = plt.plot(self.result['epoch'],self.result['train_accuracy'],'lightcoral',label='train_acc')
-        test_loss = plt.plot(self.result['epoch'],self.result['test_accuarcy'],'lightseagreen',label='test_acc')
+        train_Acc = plt.plot(self.result['epoch'],self.result['train_accuracy'],'lightcoral',label='train_acc')
+        test_Acc = plt.plot(self.result['epoch'],self.result['test_accuarcy'],'lightseagreen',label='test_acc')
         plt.title('Acc_figure')
         plt.xlabel('Epoch')
         plt.ylabel('Acc')
         plt.legend()
-        plt.savefig(os.path.join(self.result_path,self.model_file_name+'_Acc_figure.jpg'),dpi = 2000)
+        plt.savefig(os.path.join(self.result_path,self.model_file_name+'_Acc_figure.jpg'),dpi = 1500)
 
     def to_Tensor(self, x):
         return x.type(torch.FloatTensor).to(self.device)
--- myversion/utils.py
+++ myversion/utils.py
@@ -5,7 +5,7 @@ import logging
 import pickle
 import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
 
 
 def get_opt(opt_path):
@@ -45,70 +45,92 @@ def get_logger(logfile, format_str="%(asctime)s [%(pathname)s:%(lineno)s - %(lev
     return logger
 
 
-def load_dataset(data_conf):
-    train_years = data_conf['train_list']
-    test_years = data_conf['test_list']
 
-    def load_pickle(years):
+class DataLoader_Generate:
+    def __init__(self,train_conf,data_conf):
+        self.train_conf = train_conf
+        self.data_conf = data_conf
+        self.Data = self.Load_dataset()
+
+    def Load_dataset(self):
+        '''
+        返回字典dataset 键值:train和test 各存储 一个Dataframe  
+        '''
+        Data = {}
+        Data['train'] = self.pickle2DF(self.data_conf['train_list'])
+        Data['test'] = self.pickle2DF(self.data_conf['test_list'])
+        #可能的进一步处理
+        if self.data_conf['dataset_type'] == 1:
+            #原始数据处理方式
+            pass
+        elif self.data_conf['dataset_type'] == 2:
+            #按照时间合并同一天的数据，设置batch为1，然后在trainer内将弹出数据去掉第一个维度
+            def day_data(ori_df):
+                data_x = []
+                data_y = []
+                data_t = []
+                dic = {}
+                time_list = sorted(
+                    set(ori_df['day']), key=list(ori_df['day']).index)
+                for day in time_list:
+                    res = ori_df[ori_df['day'] == day]
+                    data_x.append(np.array(list(res['x'])))
+                    data_y.append(np.array(list(res['y'])))
+                    data_t.append(np.array(list(res['t'])))
+                dic['x']=data_x
+                dic['y']=data_y
+                dic['t']=data_t
+                return pd.DataFrame(dic)
+            Data['train'] = day_data(Data['train'])
+            Data['test'] = day_data(Data['test'])
+        return Data
+
+    def pickle2DF(self,year_list):
+        '''
+        将year_list内所有pickle字典数据拼接后，封装成Dataframe类型返回
+        '''
         data_dic = None
-        for y in years:
-            with open(os.path.join(data_conf['datapath'],
-                                   'v1_T'+str(data_conf['T'])+'_yb1_%s.pickle' % (y)), 'rb') as fp:
+        for y in year_list:
+            with open(os.path.join(self.data_conf['datapath'],
+                                'v1_T'+str(self.data_conf['T'])+'_yb1_%s.pickle' % (y)), 'rb') as fp:
                 dataset = pickle.load(fp)
 
-            if data_dic is None:
-                data_dic = {}
-                data_dic['x'] = list(dataset['x'])
-                data_dic['y'] = list(dataset['y'])
-                data_dic['t'] = list(dataset['t'])
-                data_dic['day'] = list(dataset['day'])
-            else:
-                data_dic['x'] = data_dic['x'] + list(dataset['x'])
-                data_dic['y'] = data_dic['y'] + list(dataset['y'])
-                data_dic['t'] = data_dic['t'] + list(dataset['t'])
-                data_dic['day'] = data_dic['day'] + list(dataset['day'])
-        return data_dic
-    dataset = {}
-    dataset['train'] = load_pickle(train_years)
-    dataset['test'] = load_pickle(test_years)
-
-    return dataset
-
-
-def Dataset_generate(dataset_type, *arg):
-    '''
-    在里面定义好自己所需要的dataset类
-    '''
-    if not isinstance(dataset_type, int):
-        raise Exception('请输入int型dataset_type!')
-    if dataset_type == 1:
+                if data_dic is None:
+                    data_dic = {}
+                    data_dic['x'] = list(dataset['x'])
+                    data_dic['y'] = list(dataset['y'])
+                    data_dic['t'] = list(dataset['t'])
+                    data_dic['day'] = list(dataset['day'])
+                else:
+                    data_dic['x'] = data_dic['x'] + list(dataset['x'])
+                    data_dic['y'] = data_dic['y'] + list(dataset['y'])
+                    data_dic['t'] = data_dic['t'] + list(dataset['t'])
+                    data_dic['day'] = data_dic['day'] + list(dataset['day'])
+        return pd.DataFrame(data_dic)
+    
+    def DF2DataLoader(self,DF):
         class dataset(Dataset):
-            def __init__(self, data):
-                self.data = data[['x', 'y', 't']]
+            def __init__(self, df):
+                self.df = df[['x', 'y', 't']]
 
             def __getitem__(self, index):
                 # 返回的目标是0 ,1
-                return self.data.iloc[index]['x'], self.data.iloc[index]['y'], self.data.iloc[index]['t']
+                return self.df.iloc[index]['x'], self.df.iloc[index]['y'], self.df.iloc[index]['t']
 
             def __len__(self):
-                return len(self.data['t'])
+                return len(self.df['t'])
+        return DataLoader(dataset(DF),batch_size=self.train_conf['batch'], shuffle=False)
 
-    elif dataset_type == 2:
-        class dataset(Dataset):
-            def __init__(self, ori_data):
-                self.data = []
-                self.time_list = sorted(
-                    set(ori_data['day']), key=list(ori_data['day']).index)
-                for day in self.time_list:
-                    res = ori_data[ori_data['day'] == day]
-                    self.data.append((np.array(list(res['x'])), np.array(list(res['y'])), np.array(list(res['t']))))
-            def __getitem__(self, index):
-                return self.data[index][0],self.data[index][1],self.data[index][2]
+    def Get_TestDataLoader(self):
+        return self.DF2DataLoader(self.Data['test'])
 
-            def __len__(self):
-                return len(self.time_list)
+    def Get_Train_ValLoader(self):
+        Train_data = self.Data['train'].sample(frac=1-self.train_conf['split'])
+        Train_data.sort_index(inplace=True)
+        Val_data = self.Data['train'][~self.Data['train'].index.isin(
+            Train_data.index)]
+        Val_data.sort_index(inplace=True)
 
-    else:
-        raise ValueError('没有该类型的dataset，请去utils.py中Dataset_generate函数内定义!')
-
-    return dataset(*arg)
+        TrainDataloader = self.DF2DataLoader(Train_data)
+        ValDataloader = self.DF2DataLoader(Val_data)
+        return TrainDataloader, ValDataloader
